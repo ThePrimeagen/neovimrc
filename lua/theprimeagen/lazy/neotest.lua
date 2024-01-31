cd1167a72240b2052eb182f2a5f27b29be1ec627
mpaulson <the.primeagen@gmail.com>
2024-01-31 09:39:50 -0700

    feat: added running the entire file for neotest

--- lua/theprimeagen/lazy/neotest.lua
+++ lua/theprimeagen/lazy/neotest.lua
@@ -24,6 +24,10 @@ return {
             vim.keymap.set("n", "<leader>tc", function()
                 neotest.run.run()
             end)
+
+            vim.keymap.set("n", "<leader>tf", function()
+                neotest.run.run(vim.fn.expand("%"))
+            end)
         end,
     },
 }
