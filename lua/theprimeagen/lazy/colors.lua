63e4f01a8423851df8f0b098b2ca0df0ba49c56e
proudmuslim-dev <69869443+proudmuslim-dev@users.noreply.github.com>
2024-02-01 08:06:32 +0000

    Fix Rosé Pine config
    Rosé Pine had some breaking changes recently

--- lua/theprimeagen/lazy/colors.lua
+++ lua/theprimeagen/lazy/colors.lua
@@ -36,6 +36,9 @@ return {
         config = function()
             require('rose-pine').setup({
                 disable_background = true,
+                styles = {
+                    transparency = true
+                }
             })
 
             vim.cmd("colorscheme rose-pine")
